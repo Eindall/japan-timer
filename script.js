16e8e1e290f3073054ee23e8f21c9e15939377a7
Thomas Marquis <thomas.marquis@epitech.eu>
2019-09-05 10:38:17 +0200

    Loljaj

---
+++ script.js
@@ -0,0 +1,27 @@
+var countdownDate = new Date("Mar 15, 2020 00:00:00").getTime();
+
+var months, days, hours, minutes, seconds;
+
+var interval = setInterval(function() {
+  var now = new Date().getTime();
+  
+  var distance = parseInt((countdownDate - now) / 1000);
+
+  if (distance >= 0) {
+    months = parseInt(distance / 2592000);
+    distance = distance % 2592000;
+
+    days = parseInt(distance / 86400);
+    distance = distance % 86400;
+
+    hours = parseInt(distance / 3600);
+    distance = distance % 3600;
+
+    minutes = parseInt(distance / 60);
+    distance = distance % 60;
+
+    seconds = parseInt(distance);
+
+    document.getElementById("display").innerHTML = "On est au Japon dans " + months + " mois, " + days + " jours, " + hours + " heures, " + minutes + " minutes et " + seconds + " secondes !";
+  }
+}, 1000);
